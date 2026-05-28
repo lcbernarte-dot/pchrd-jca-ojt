ecd90654abddb085f086a0222fdb1fd26b37c571
Leerobin Irlandez <lcbernarte@paterostechnologicalcollege.edu.ph>
2026-05-28 14:41:43 +0800

    database_continunation

---
+++ views/UserViews.php
@@ -0,0 +1,28 @@
+<div class="card">
+
+        <h2>Users Table</h2>
+
+    <table>
+
+            <tr>
+                <th>ID</th>
+                <th>Email</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+            </tr>
+
+            <?php
+            while($row = $userResult->fetch_assoc()){
+            ?>
+
+            <tr>
+                <td><?php echo $row['id']; ?></td>
+                <td><?php echo $row['email']; ?></td>
+                <td><?php echo $row['first_name']; ?></td>
+                <td><?php echo $row['last_name']; ?></td>
+            </tr>
+
+            <?php } ?>
+
+    </table>
+</div>
