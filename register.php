afb158b49667e59605356e1220edbb246aaf0228
Leerobin Irlandez <lcbernarte@paterostechnologicalcollege.edu.ph>
2026-07-14 16:11:28 +0800

    ALL WORKING

--- register.php
+++ register.php
@@ -12,7 +12,7 @@ session_start();
 
 <div class="container">
 
-    <div class="card">
+    <div class="card register-card">
 
         <h2>Create Account</h2>
 
@@ -51,12 +51,41 @@ session_start();
                 required
             >
 
-            <input
-                type="password"
-                name="password"
-                placeholder="Password"
-                required
-            >
+            <div class="password-group">
+
+                <input
+                    type="password"
+                    id="password"
+                    name="password"
+                    placeholder="Password"
+                    oninput="toggleEye('password','eye1')"
+                    required
+                >
+                <span id="eye1" class="toggle-password" onclick="togglePassword('password','eye1')">👁</span>
+
+            </div>
+
+            <div id="confirmPasswordBox" style="display:none;">
+
+                <div class="password-group">
+                    <input
+                        type="password"
+                        id="confirm_password"
+                        name="confirm_password"
+                        placeholder="Re-enter Password"
+                        oninput="toggleEye('confirm_password','eye2')"
+                        required
+                    >
+
+                    <span id="eye2"
+                        class="toggle-password"
+                        onclick="togglePassword('confirm_password','eye2')">
+                        👁
+                    </span>
+
+                </div>
+
+            </div>
 
             <input
                 type="hidden"
@@ -79,6 +108,49 @@ session_start();
     </div>
 
 </div>
+
+    <script>
+    function toggleEye(inputId, eyeId){
+
+        const input = document.getElementById(inputId);
+        const eye = document.getElementById(eyeId);
+
+        if(input.value.length > 0){
+            eye.style.display = "block";
+        }else{
+            eye.style.display = "none";
+            input.type = "password";
+            eye.textContent = "👁";
+        }
+    }
+
+    function togglePassword(inputId, eyeId){
+
+        const input = document.getElementById(inputId);
+        const eye = document.getElementById(eyeId);
+
+        if(input.type === "password"){
+            input.type = "text";
+            eye.textContent = "⌣";
+        }else{
+            input.type = "password";
+            eye.textContent = "👁";
+        }
+    }
+
+        const password = document.getElementById("password");
+        const confirmBox = document.getElementById("confirmPasswordBox");
+
+    password.addEventListener("blur", function () {
+
+        if (this.value.trim() !== "") {
+            confirmBox.style.display = "block";
+        } else {
+            confirmBox.style.display = "none";
+        }
+
+    });
+    </script>
 
 </body>
 </html>
